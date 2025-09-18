ae5f843716b5dc4589c0b27b79bca8b9232227f1
juli4nn3 <julianne.leblancpeltier@gmail.com>
2025-09-18 03:48:59 +0000

    fixed the program I feel normal

--- Main.java
+++ Main.java
@@ -16,19 +16,6 @@ public class Main {
         int choicePython = 0;
         int choiceScratch = 0;
 
-        // for loop counters
-        int tiedCounter = 0;
-        int decisionCounter = 0;
-        int nextChoiceValue = 0;
-
-        // other variables
-        boolean isTiedScore = false;
-        String finalUserLanguage = "";
-
-        // arrays
-        int highestScore = determineChoice[0];
-        int[] tiedScore = new int[4];
-
         // create objects
         Scanner userNameInput = new Scanner(System.in);
         Scanner choiceInput = new Scanner(System.in);
@@ -69,41 +56,33 @@ public class Main {
             }
         }
 
-        for (int n : determineChoice) {
-            // n is score value of each choice
-            // decision counter is the iteration
-            // highest score is the highest value iterated
-            // if next score is greater than the previous score, then make highest score the next score
+        int[] determineChoice = {choiceJava, choiceCpp, choicePython, choiceScratch};
+        String[] userLanguage = {"Java", "C++", "Python", "Scratch"};
 
-            if (decisionCounter <= 2) {
-                nextChoiceValue = determineChoice[(decisionCounter + 1)];
-            } else {
-                nextChoiceValue = -1;
+        int highestScore = determineChoice[0];
+        for (int i = 1; i < determineChoice.length; i++) {
+            if (determineChoice[i] > highestScore) {
+                highestScore = determineChoice[i];
             }
-
-            if (n > nextChoiceValue) {
-                highestScore = n;
-                finalUserLanguage = userLanguage[decisionCounter];
-                if (n > tiedScore[0]){
-                    isTiedScore = false;
-                    tiedCounter = 0;
-                    for (int deleteTiedScore = 0; deleteTiedScore <= 3; deleteTiedScore++) {
-                        tiedScore[deleteTiedScore] = 0;
-                    }
-                }
-            } else if (n == nextChoiceValue && n > highestScore) {
-                isTiedScore = true;
-                tiedScore[tiedCounter] = highestScore;
-                finalUserLanguage += userLanguage[decisionCounter] + " ";
-                tiedCounter++;
-            }
-            decisionCounter++;
         }
 
-        if (isTiedScore == true) {
-            System.out.println("\nThere is a tie! You are the following: " + finalUserLanguage);
+        StringBuilder finalUserLanguages = new StringBuilder();
+        int tiedCount = 0;
+
+        for (int i = 0; i < determineChoice.length; i++) {
+            if (determineChoice[i] == highestScore) {
+                if (tiedCount > 0) {
+                    finalUserLanguages.append(", ");
+                }
+                finalUserLanguages.append(userLanguage[i]);
+                tiedCount++;
+            }
+        }
+
+        if (tiedCount == 1) {
+            System.out.println(userName + ", You are " + finalUserLanguages + ".");
         } else {
-            System.out.println(userName + ", You are " + finalUserLanguage);
+            System.out.println("There is a tie! " + userName + ", You are: " + finalUserLanguages + ".");
         }
 
         // closes all input Scanner objects
@@ -117,7 +96,7 @@ public class Main {
             "\n1) Do you like typing?\n", 
             "\n2) Do you have a lot of experience with coding?\n", 
             "\n3) Do you enjoy simplicity?\n", 
-            "\n4) DO you plan on going into a programming field?\n", 
+            "\n4) Do you plan on going into a programming field?\n", 
             "\n5) Do you like classes?\n", 
             "\n6) What's your favourite field of programming?\n", 
             "\n7) How old are you?\n", 
@@ -128,64 +107,64 @@ public class Main {
 
         final String choices[] = {
             """
-            A) I've tested my typing speed before (it's fast)
-            B) I like typing, but I won't tell anyone
-            C) I'm competitive, but I'm not very good
-            D) No, the mouse button was made for me
+                A) I've tested my typing speed before (it's fast)
+                B) I like typing, but I won't tell anyone
+                C) I'm competitive, but I'm not very good
+                D) No, the mouse button was made for me
             """, 
             """
-            A) I was born making classes
-            B) With age comes experience
-            C) I made a calculator once
-            D) I can do the minecraft cheat codes
+                A) I was born making classes
+                B) With age comes experience
+                C) I made a calculator once
+                D) I can do the minecraft cheat codes
             """, 
             """
-            A) Simplicity is laziness
-            B) Back in my day we didn't have simplicity
-            C) Work smarter, not harder
-            D) I like it as easy as can be
+                A) Simplicity is laziness
+                B) Back in my day we didn't have simplicity
+                C) Work smarter, not harder
+                D) I like it as easy as can be
             """, 
             """
-            A) I am the programming field
-            B) Yeah, I have a Windows 7
-            C) Yeah, I'm just getting into the field
-            D) What's programming?
+                A) I am the programming field
+                B) Yeah, I have a Windows 7
+                C) Yeah, I'm just getting into the field
+                D) What's programming?
             """, 
             """
-            A) Whatever C++ said
-            B) I AM classes
-            C) Nah, we don't do that here
-            D) My favourite class is art
+                A) Whatever C++ said
+                B) I AM classes
+                C) Nah, we don't do that here
+                D) My favourite class is art
             """, 
             """
-            A) I love making andriod apps
-            B) I love game development
-            C) I love web development and data analysis
-            D) The soccer field :D
+                A) I love making andriod apps
+                B) I love game development
+                C) I love web development and data analysis
+                D) The soccer field :D
             """, 
             """
-            A) I'm in my prime
-            B) I see the light
-            C) I'm young with a dream
-            D) I'm not supposed to tell my age to strangers
+                A) I'm in my prime
+                B) I see the light
+                C) I'm young with a dream
+                D) I'm not supposed to tell my age to strangers
             """, 
             """
-            A) I have a 20 step skincare routine
-            B) I use an 8 in 1, it gets the job done
-            C) I have a loofa
-            D) I take bubble baths
+                A) I have a 20 step skincare routine
+                B) I use an 8 in 1, it gets the job done
+                C) I have a loofa
+                D) I take bubble baths
             """, 
             """
-            A) I talk business
-            B) I love politics
-            C) Everyone wants to know what I'm up to
-            D) I'm kinda clueless, but I like to join in on the fun
+                A) I talk business
+                B) I love politics
+                C) Everyone wants to know what I'm up to
+                D) I'm kinda clueless, but I like to join in on the fun
             """, 
             """
-            A) Wagyu
-            B) Green bean casserole
-            C) Penne pasta
-            D) Chicken fingers
+                A) Wagyu
+                B) Green bean casserole
+                C) Penne pasta
+                D) Chicken fingers
             """
         };
 
